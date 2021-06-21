cea89dbd96686aac2a3e4a21768a0564ecbfd57e
riccardo.scanavacca <riccardo.scanavacca@tigerspike.com>
2021-06-21 21:39:10 +0100

    Create graphql api

--- amicii-backend-cdk/lib/amicii-backend-cdk-stack.ts
+++ amicii-backend-cdk/lib/amicii-backend-cdk-stack.ts
@@ -1,9 +1,18 @@
 import * as cdk from '@aws-cdk/core';
+import * as appsync from '@aws-cdk/aws-appsync'
 
 export class AmiciiBackendCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, 'AmiciiApi', {
+      name: 'cdk-amicii-appsync-api',
+      schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.USER_POOL
+        }
+      }
+    })
   }
 }
