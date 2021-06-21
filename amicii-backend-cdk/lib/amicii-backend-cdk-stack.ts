bafb235942068860f72e9620d44c521c59881fc1
riccardo.scanavacca <riccardo.scanavacca@tigerspike.com>
2021-06-21 22:44:37 +0100

    Create lambda for interacting with db

--- amicii-backend-cdk/lib/amicii-backend-cdk-stack.ts
+++ amicii-backend-cdk/lib/amicii-backend-cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync'
 import * as ec2 from '@aws-cdk/aws-ec2'
 import * as rds from '@aws-cdk/aws-rds'
+import * as lambda from '@aws-cdk/aws-lambda'
 import {Duration} from "@aws-cdk/aws-appsync/node_modules/@aws-cdk/core/lib/duration";
 
 
@@ -28,5 +29,44 @@ export class AmiciiBackendCdkStack extends cdk.Stack {
       vpc: vpc,
       scaling: { autoPause: Duration.seconds(0) }
     })
+
+    const userFn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: new lambda.AssetCode('lambda-fns'),
+      handler: 'index.handler',
+      memorySize: 1024,
+      environment: {
+        CLUSTER_ARN: cluster.clusterArn,
+        SECRET_ARN: cluster.secret?.secretArn || '',
+        DB_NAME: 'AmiciiDB',
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1'
+      }
+    })
+
+    cluster.grantDataApiAccess(userFn)
+
+    const lambdaDs = api.addLambdaDataSource('lambdaDataSource', userFn)
+
+    lambdaDs.createResolver({
+      typeName: 'Query',
+      fieldName: 'getCandidates'
+    })
+
+    lambdaDs.createResolver({
+      typeName: 'Query',
+      fieldName: 'getMatches'
+    })
+
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createUser'
+    })
+
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'updateUser'
+    })
+
+
   }
 }
