f97bef2b7bfc0bf872053e6f32477cee40443e04
riccardo.scanavacca <riccardo.scanavacca@tigerspike.com>
2021-07-08 11:04:25 +0100

    Create App's basic navigation

--- amicii-app-rn/App.tsx
+++ amicii-app-rn/App.tsx
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, Text, View } from 'react-native';
+
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+            name="Tab"
+            options={ {animationEnabled: false, headerShown: false} }
+        >
+          {() => (
+              <Tab.Navigator
+                tabBarOptions={{
+                  activeTintColor: "red",
+                  inactiveTintColor: "dark_gray",
+                  showLabel: false,
+                  labelStyle: { fontSize: 14, textTransform: 'uppercase', paddingTop: 10 },
+                  style: styles.tabBar
+                }}
+              >
+
+
+              </Tab.Navigator>
+          )}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,4 +43,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tabBar: {
+    backgroundColor: 'WHITE',
+    marginBottom: 0,
+    borderTopWidth: 0,
+    shadowColor: 'black',
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    shadowOffset: { height: 0, width: 0 }
+  }
 });
