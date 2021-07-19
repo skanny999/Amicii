5350ba307e8e08914bacb68253d87fc4d3576d37
riccardo.scanavacca <riccardo.scanavacca@tigerspike.com>
2021-07-19 16:13:21 +0100

    App - Fix Icon

--- amicii_mobile_rn/src/components/Icon.tsx
+++ amicii_mobile_rn/src/components/Icon.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 import { IconType } from '../types'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+Ionicons.loadFont().then()
 
 const Icon = ({ name, size, color, style }: IconType) => (
-    <Icon name={name} size={size} color={color} style={style}/>
+    <Ionicons name={name} size={size} color={color} style={style}/>
 )
 
 export default Icon
