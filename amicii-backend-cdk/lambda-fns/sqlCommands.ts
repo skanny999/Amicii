f33d27092a738a6740feb9c08dfd37722696c4d4
riccardo.scanavacca <riccardo.scanavacca@tigerspike.com>
2021-08-05 11:56:34 +0100

    Backend - update sql queries

--- amicii-backend-cdk/lambda-fns/sqlCommands.ts
+++ amicii-backend-cdk/lambda-fns/sqlCommands.ts
@@ -1,5 +1,5 @@
 //Tables
-export const createUsersTableQuery = `CREATE TABLE User (
+export const createUsersTableQuery = `CREATE TABLE IF NOT EXISTS User (
     id VARCHAR(191) NOT NULL,
     username VARCHAR(191) NOT NULL DEFAULT '',
     age INTEGER NOT NULL DEFAULT 0,
@@ -13,7 +13,7 @@ export const createUsersTableQuery = `CREATE TABLE User (
     PRIMARY KEY (id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`
 
-export const createLikesTableQuery = `CREATE TABLE Likes (
+export const createLikesTableQuery = `CREATE TABLE IF NOT EXISTS Likes (
     id INTEGER NOT NULL AUTO_INCREMENT,
     userId VARCHAR(191) NOT NULL,
     likedUserId VARCHAR(191) NOT NULL,
@@ -21,7 +21,7 @@ export const createLikesTableQuery = `CREATE TABLE Likes (
     PRIMARY KEY (id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`
 
-export const createDislikesTableQuery = `CREATE TABLE Dislikes (
+export const createDislikesTableQuery = `CREATE TABLE IF NOT EXISTS Dislikes (
     id INTEGER NOT NULL AUTO_INCREMENT,
     userId VARCHAR(191) NOT NULL,
     dislikedUserId VARCHAR(191) NOT NULL,
@@ -29,7 +29,7 @@ export const createDislikesTableQuery = `CREATE TABLE Dislikes (
     PRIMARY KEY (id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`
 
-export const createFeaturesTableQuery = `CREATE TABLE Features (
+export const createFeaturesTableQuery = `CREATE TABLE IF NOT EXISTS Features (
     id INTEGER NOT NULL AUTO_INCREMENT,
     emoji VARCHAR(191) NOT NULL,
 
@@ -37,7 +37,7 @@ export const createFeaturesTableQuery = `CREATE TABLE Features (
     PRIMARY KEY (id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`
 
-export const createFeatureRelationQuery = `CREATE TABLE _FeaturesToUser (
+export const createFeatureRelationQuery = `CREATE TABLE IF NOT EXISTS _FeaturesToUser (
     A INTEGER NOT NULL,
     B VARCHAR(191) NOT NULL,
 
@@ -49,16 +49,16 @@ export const addFeatureToUserForeignKey = `ALTER TABLE _FeaturesToUser ADD FOREI
 
 export const addUserToFeatureForeignKey = `ALTER TABLE _FeaturesToUser ADD FOREIGN KEY (B) REFERENCES User(id) ON DELETE CASCADE ON UPDATE CASCADE;`
 
-export const addDefaultFeatures = `INSERT INTO Features VALUES(1, 'PL1');
-INSERT INTO Features VALUES(2, 'PL2');
-INSERT INTO Features VALUES(3, 'PL3');
-INSERT INTO Features VALUES(4, 'PL4');
-INSERT INTO Features VALUES(5, 'PL5');
-INSERT INTO Features VALUES(6, 'PL6');
-INSERT INTO Features VALUES(7, 'PL7');
-INSERT INTO Features VALUES(8, 'PL8');
-INSERT INTO Features VALUES(9, 'PL9');
-INSERT INTO Features VALUES(10, 'PL10');`
+export const addDefaultFeatures = `INSERT IGNORE INTO Features VALUES(1, 'PL1');
+INSERT IGNORE INTO Features VALUES(2, 'PL2');
+INSERT IGNORE INTO Features VALUES(3, 'PL3');
+INSERT IGNORE INTO Features VALUES(4, 'PL4');
+INSERT IGNORE INTO Features VALUES(5, 'PL5');
+INSERT IGNORE INTO Features VALUES(6, 'PL6');
+INSERT IGNORE INTO Features VALUES(7, 'PL7');
+INSERT IGNORE INTO Features VALUES(8, 'PL8');
+INSERT IGNORE INTO Features VALUES(9, 'PL9');
+INSERT IGNORE INTO Features VALUES(10, 'PL10');`
 
 
 // User 
