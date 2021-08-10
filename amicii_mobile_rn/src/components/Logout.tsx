471e9a7d003b88731a534b259395da1e11b0e30d
riccardo.scanavacca <riccardo.scanavacca@tigerspike.com>
2021-08-10 15:45:35 +0100

    App - Add logout button

---
+++ amicii_mobile_rn/src/components/Logout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text, TouchableOpacity} from 'react-native'
+import styles, { GRAY } from "../assets/styles";
+import Icon from './Icon'
+
+
+const Logout = (props: {handlePress: () => void}) => (
+    <TouchableOpacity style={styles.buttonGeneral} onPress={props.handlePress}>
+        <Text style={styles.buttonText}>
+            <Icon name='log-out-outline' size={13} color={GRAY}/> Logout
+        </Text>
+    </TouchableOpacity>
+)
+
+export default Logout
